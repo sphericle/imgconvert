56876404b1956db1aff19ebf53746259f2812c9e
sphericle <jackb2033@gmail.com>
2024-11-22 20:06:52 -0500

    skldjfdfs

---
+++ main.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import re
+
+import os
+
+base_path = 'input/'
+
+i = 0
+
+for filename in os.listdir(base_path):
+    file_path = os.path.join(base_path, filename)
+    if os.path.isfile(file_path):
+        no_ext = filename.split('.')[0]
+        
+        print('Converted ' + no_ext)
+        
+        try:
+            i += 1
+            im = Image.open(file_path).convert('RGB')
+            im.save(f'output/converted {no_ext} {i}.png', "png")
+        except:
+            print('File not found')
